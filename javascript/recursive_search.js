e2b948c6a6741e8caf401ce7fcb225039a673d3d
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-10-30 04:58:57 -0600

    skeleton + pseudocode

--- javascript/recursive_search.js
+++ javascript/recursive_search.js
@@ -1,5 +1,14 @@
 function recursiveSearch(arr, target) {
   // type your code here
+  console.log(arr)
+  if (arr.length > 0) {
+    if (arr[0] === target) {
+      return true;
+    }
+    arr.shift()
+    console.log(arr);
+  }
+  else return false;
 }
 
 if (require.main === module) {
@@ -16,4 +25,13 @@ if (require.main === module) {
 module.exports = recursiveSearch;
 
 // Please add your pseudocode to this file
+/**
+ * another kind of awkward problem, where the obvious solution would be to just call it
+ * iteratively since searching arrays is set up so well for that
+ * anyway,
+ * given an array and a target value
+ * test the first value in the array with the target
+ * if the same, return true
+ * if not, repeat with the array - that value (remove first value)
+ */
 // And a written explanation of your solution
